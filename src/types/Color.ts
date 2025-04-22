6791a4c0a7a165051e7228ae4f00fbc28b905cfc
Daniel Arcé <daniel.arce@gmail.com>
2025-04-21 23:05:06 -0400

    scaffold color model type

---
+++ src/types/Color.ts
@@ -0,0 +1,26 @@
+export type ColorModel = 'rgb' | 'hsl' | 'hsv';
+
+export type RGB = Readonly<{
+    r: number; // 0 - 255
+    g: number; // 0 - 255
+    b: number; // 0 - 255
+    a?: number; // 0 - 255
+}>;
+
+export type HSL= Readonly<{
+    h: number; // 0 - 360
+    s: number; // 0 - 100
+    l: number; // 0 - 100
+    a?: number; // 0 - 100
+}>;
+
+export type HSV = Readonly<{
+    h: number; // 0 - 360
+    s: number; // 0 - 100
+    v: number; // 0 - 100
+}>;
+
+export type Color =
+    | Readonly<{ model: 'rgb'; value: RGB}>
+    | Readonly<{ model: 'hsl'; value: HSL}>
+    | Readonly<{ model: 'hsv'; value: HSV}>;
